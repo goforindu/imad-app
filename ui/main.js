395ec8b2c8b539ee3fd4d497f16baf9bea8af9d5
goforindu <30650939+goforindu@users.noreply.github.com>
2017-08-17 23:10:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,12 @@ button.onclick=function(){
     request.open('GET',"http://goforindu.imad.hasura-app.io/counter",true);
     request.send(null);
 };
+
+//submit name
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    //make a request to server send name as data
+    //get the reponse as alist of name submit and render it
+}
