1fddc43a678644cb9e38ec8ebd6cca281fc14998
goforindu <30650939+goforindu@users.noreply.github.com>
2017-08-17 16:13:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,22 @@
 console.log('Loaded!');
 //counter code
 var button=document.getElementById("counter");
-var counter=0;
+
 button.onclick=function(){
-    //make a request to a counter endpoint
+    //create a request to a counter endpoint
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
     //get the response and store in a variable
-    //render it in a correct span
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    
+    //make a request
+    request.open('GET',"http://goforindu.imad.hasura-app.io/counter",true);
+    request.send(null);
 };
