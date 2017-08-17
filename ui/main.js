64ceebe9fd38c9b947dcf1512bee8cdfa4450a24
goforindu <30650939+goforindu@users.noreply.github.com>
2017-08-17 13:10:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ button.onclick=function(){
     //render it in a correct span
     counter=counter+1;
     var span=document.getElementById("count");
-    span.innerHtml=counter.toString();
+    span.innerHTML=counter.toString();
 };
