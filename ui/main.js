89508e8b406903a10648e6bdc1b6ff47c02951b0
goforindu <30650939+goforindu@users.noreply.github.com>
2017-08-17 13:06:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+//counter code
+var counter=0;
+var button=document.getElementById("counter");
+button.onClick=function(){
+    //make a request to a counter endpoint
+    //get the response and store in a variable
+    //render it in a correct span
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHtml=counter.toString();
+};
